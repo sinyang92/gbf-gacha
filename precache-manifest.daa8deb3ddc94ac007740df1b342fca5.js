b2614a0c315442ecf0dbc9f148c94b101116e2e7
sinyang <svenyang92@gmail.com>
2020-07-23 23:54:58 +1000

    Updates

---
+++ precache-manifest.daa8deb3ddc94ac007740df1b342fca5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bc70bf46953cf9a25e81784a666d630",
+    "url": "/gbf-gacha/index.html"
+  },
+  {
+    "revision": "cd1153c42da5ba078007",
+    "url": "/gbf-gacha/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1531292993cc9e1ce2dc",
+    "url": "/gbf-gacha/static/js/2.931cdd8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gbf-gacha/static/js/2.931cdd8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd1153c42da5ba078007",
+    "url": "/gbf-gacha/static/js/main.8e660deb.chunk.js"
+  },
+  {
+    "revision": "283902cd9892ae7df0b3",
+    "url": "/gbf-gacha/static/js/runtime-main.a81446d0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gbf-gacha/static/media/logo.5d5d9eef.svg"
+  }
+]);
